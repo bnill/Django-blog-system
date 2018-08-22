5b631333686380d60dc08b5cb7d00bf818ad6b9e
Yuyang Zhang <zhangyy@YuyangmatoMacBook-Pro.local>
2018-08-22 01:13:19 -0400

    detail page

--- blog/src/blog/urls.py
+++ blog/src/blog/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html')),
     url(r'^restaurants/$', RestaurantListView.as_view()),
     #url(r'^restaurants/(?P<slug>\w+)/$', RestaurantListView.as_view()),
-    url(r'^restaurants/(?P<rest_id>\w+)/$', RestaurantDetailView.as_view()),
+    url(r'^restaurants/(?P<slug>[\w-]+)/$', RestaurantDetailView.as_view()),
 ]
